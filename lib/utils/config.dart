56dd3b5eec3ba585d303fd784353d75562aee877
Satbir Singh <Satbir-Laptop@BSINGH-LAPTOP.iolcp.com>
2025-11-12 20:35:26 +0530

    C

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const String icecastBaseUrl = 'https://play.global.audio';
-  static const String liveStreamUrl = 'https://play.global.audio/novahi.aac';
+  static const String icecastBaseUrl = 'http://10.102.176.192:8000/';
+  static const String liveStreamUrl = 'http://10.102.176.192:8000/stream';
   static const String metadataUrl =
       '$icecastBaseUrl/status-json.xsl'; // Example for Icecast metadata
   static const String programsApiUrl =
